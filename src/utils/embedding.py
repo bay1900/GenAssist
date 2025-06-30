881d158e6a37435934ea900e96c44b93e15e392b
bay1900 <bayauscoffeeit@gmail.com.com>
2025-06-30 12:01:00 +1000

    removed api key

--- src/utils/embedding.py
+++ src/utils/embedding.py
@@ -4,11 +4,56 @@ from langchain_openai import OpenAIEmbeddings
 from src.utils import yaml, env
 
 
-def embed( llm = "openai"):    
+def embed_model ( llm, api): 
+
+    os.environ[api]
+    EMBED_MAPPING = { 
+        "openai"   : OpenAIEmbeddings,
+        "mistral"  : MistralAIEmbeddings
+    }
+    
+    model = classmethod
+    try: 
+        model = EMBED_MAPPING.get( llm )
+        if model is None: 
+            raise ValueError
+    except ValueError as e: 
+        err = f"Error : Unsupport Embeding Model {llm}"
+        print( err ) 
+    except KeyError as e: 
+        err = f"Error : Unsupport Embeding Model {llm}"
+        print( err ) 
+           
+    return model 
+    
+def embed( llm ):  
+    
+    payload = { 
+                "status": False,
+                "data": "",
+                "error": ""
+              }  
+    
+    # PATH
     config_path = env.read( "MODEL_CONFIG")
     
     # PROPERTY 
-    property = yaml.read( config_path )
+    property    = yaml.read( config_path )
+    llm_propery = ""
+    try:
+        llm_propery = property["data"][llm]
+    except KeyError as e : 
+        err = f"Error : Unsupport Model {e}",
+        print( err )
+        payload["error"] = err
+        return payload
+            
+    # VALUE
+    api    = llm_propery["api"]
+    vector = llm_propery["vector"]
+    model_chat  = llm_propery["model_chat"]
     
     
-    print ( " property : ", property )
+    embed = embed_model( llm, api )
+    print ( "embed : ", embed )
+    return payload
