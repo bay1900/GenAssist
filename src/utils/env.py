2026f5ffd065787154db9e1b4a6be065e642a5f8
bay1900 <bayauscoffeeit@gmail.com.com>
2025-07-06 11:18:13 +1000

    added envread handler

--- src/utils/env.py
+++ src/utils/env.py
@@ -1,8 +1,26 @@
 import os 
 from dotenv import load_dotenv
 
-
+from src.utils.logger import setup_logger
+logger = setup_logger(__name__, log_file='logs/file_operations.log') 
+  
 async def read ( key ):
-    load_dotenv()
-    data = os.getenv( key )
-    return str( data )
+    payload = { 
+                "status": False,
+                "data": "",
+                "msg": ""
+              }
+    try:
+        load_dotenv() # LOAD ENV VARIABLE
+        data = os.getenv(key)
+
+        if data is not None:
+            payload["status"] = True
+            payload["data"]   = str(data)
+        else:
+            payload["msg"] = f"Warning: Environment variable '{key}' not found." 
+            logger.warning(f"Warning: Environment variable '{key}' not found." )
+    except Exception as e:
+        payload["msg"] = f"An error occurred while reading environment variable '{key}': {e}"
+
+    return payload
